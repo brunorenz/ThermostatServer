be3fdc20fdc44a6fc60cc23967d3350223994741
brunorenz <65bruno@gmail.com>
2020-11-07 18:12:49 +0100

    Rebase from V2

--- startTerm.sh
+++
@@ -1,7 +0,0 @@
-#
-
-thermPath=/home/bruno/software/node
-rm ${thermPath}/MainServer.pid
-
-nohup nodejs ${thermPath}/ThermServer/MainServer.js &
-echo $! >>${thermPath}/MainServer.pid
--- term.sh
+++
@@ -1,6 +0,0 @@
-#
-
-rm /share/NODE/MainServer.pid
-
-nohup node /share/NODE/ThermServer/MainServer.js &
-echo $! >>/share/NODE/MainServer.pid
